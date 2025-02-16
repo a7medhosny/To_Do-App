c35858b4aabca9eb744ea869c74e15c121b56afe
a7medhosny <a7meddhosnyy@gmail.com>
2025-02-16 21:41:01 +0200

    create struc

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/core/utils/app_strings.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppStrings.tilte,
+      home: Scaffold(),
+    );
+  }
+}
